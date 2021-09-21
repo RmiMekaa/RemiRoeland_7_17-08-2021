b52483428639c2db6705f4d091802628bf9595f1
Rémi <roeland.remi@yahoo.fr>
2021-09-21 17:00:51 +0200

    Ajout fonction pour fermer les dropdowns si on clique à l'exterieur de ceux-ci

--- Javascript/dropdownEventsHandler.js
+++ Javascript/dropdownEventsHandler.js
@@ -4,115 +4,98 @@ export class DropdownEventsHandler {
     this.dropdowns = document.querySelectorAll('details');
     this.outFocusClose();
     this.arrowClose();
-    //this.dropdownOpen();
-    this.filterList();
-  }
-
-  dropdownOpen(){
-    let input = document.getElementById('searchBar');
-    console.log(input);
-    this.dropdowns.forEach(dropdown => {
-      dropdown.addEventListener('click', function() {
-        console.log('hello');
-        input.focus();
-      })
-        
-    })
+    this.searchInList();
   }
 
   /**
-   * Évènements au clic sur un élément de la liste
-   * @return  {void}
-   */
-  listOnClick(category, value) {
-    globalThis.dataManager.addFilter(category, value);
-    this.createTag(category, value);
+     * Évènements au clic sur un élément de la liste
+     * @return  {void}
+     */
+  listClickEvents(category, value, dropdown) {
+      dataManager.addFilter(category, value);
+      dataManager.displayResults();
+      this.createTag(category, value);
+      dropdown.removeAttribute('open');
   }
-
   /**
-   * Créé un tag au dessus des dropdowns
-   *
-   * @param   {string}  category  La catégorie du tag
-   * @param   {string}  string    Le texte du tag
-   *
-   * @return  {void}
-   */
+     * Évènements au clic sur un tag
+     *
+     * @param   {string}        category  La catégorie du tag
+     * @param   {HTMLElement}   el        L'élément sur lequel le clic a été effectué
+     *
+     * @return  {void}
+     */
+  tagClickEvents(category, el) {
+      el.remove();
+      dataManager.removeFilter(category, el.textContent);  
+      dataManager.manageResults();
+  }
+  
+  /**
+     * Créé un tag et l'insère dans le DOM
+     *
+     * @param   {string}  category  La catégorie du tag
+     * @param   {string}  string    Le texte du tag
+     *
+     * @return  {void}
+     */
   createTag(category, string) {
-    const tagContainer = document.getElementById('tags-container');
-    let tag = document.createElement('li');
-    tag.innerText = string;
-    tag.className = 'tag';
-    tag.classList.add(`tag__${category}`);
-    tagContainer.appendChild(tag);
-    tag.addEventListener('click', () => {
-      this.tagOnClick(category, tag);
-    })
-  }
-
-  /**
-   * Évènements au clic sur un tag
-   *
-   * @param   {string}        category  La catégorie du tag
-   * @param   {HTMLElement}   tag       L'élément sur lequel le clic a été effectué
-   *
-   * @return  {void}
-   */
-  tagOnClick(category, tag) {
-    tag.remove();
-    globalThis.dataManager.removeFilter(category, tag.textContent);  
-  }
-
-  /**
-   * Affiche les éléments de la liste correspondants à la recherche
-   * @return  {void}  [return description]
-   */
-  filterList() {
-    const searchInput = document.querySelectorAll('.dropdown input');
-    searchInput.forEach(input => {
-      input.addEventListener('keyup', function () {
-        const searchString = input.value.toLowerCase();
-        let list = input.closest('details').querySelectorAll('ul li');
-        if (input.value.length > 0) {
-          list.forEach(el => {
-            if (!el.textContent.toLowerCase().includes(searchString)) el.style.display = 'none';
-          })
-          return;
-        }
-        list.forEach(el => el.style.display = 'inline');
+      const tagContainer = document.getElementById('tags-container');
+      let tag = document.createElement('li');
+      tag.innerText = string;
+      tag.className = 'tag';
+      tag.classList.add(`tag__${category}`);
+      tagContainer.appendChild(tag);
+      tag.addEventListener('click', () => {
+        this.tagClickEvents(category, tag);
       })
-    })
   }
 
-  //----- Close Events -----------------------------------------------------
-
   /**
-   * Ferme les dropdowns qui ne sont pas la cible du clic
-   * @return  {void} 
-   */
-  outFocusClose() {
-    const dropdowns = document.querySelectorAll('details');
-    dropdowns.forEach((targetDropdown) => {
-      targetDropdown.addEventListener("click", () => {
-        dropdowns.forEach((dropdown) => {
-          if (dropdown !== targetDropdown) {
-            dropdown.removeAttribute("open");
-            //dropdown.querySelector('summary').style.display = 'flex';
+     * Affiche les éléments de la liste correspondants à la recherche
+     * @return  {void}  [return description]
+     */
+  searchInList() {
+      const searchInput = document.querySelectorAll('.dropdown input');
+      searchInput.forEach(input => {
+        input.addEventListener('keyup', function () {
+          const searchString = input.value.toLowerCase();
+          let list = input.closest('details').querySelectorAll('ul li');
+          if (input.value.length > 0) {
+            list.forEach(el => {
+              if (!el.textContent.toLowerCase().includes(searchString)) el.style.display = 'none';
+            })
+            return;
           }
-        });
-      });
-    })
-  }
-  /**
-   * Ferme le dropdown au clic sur la flèche
-   * @return  {void} 
-   */
-  arrowClose() {
-    let closeArrows = document.querySelectorAll('.arrow-up'); 
-    closeArrows.forEach(arrow => {
-      arrow.addEventListener('click', () => {
-        arrow.closest('details').removeAttribute('open');
+          list.forEach(el => el.style.display = 'inline');
+        })
       })
-    })
   }
 
+  /**
+     * Ferme les dropdowns si un clic en dehors est détecté
+     * @return  {void} 
+     */
+  outFocusClose() {
+      const dropdowns = document.querySelectorAll('details');
+      window.addEventListener('click', function(e) {
+        dropdowns.forEach(dropdown => {
+          if (dropdown.hasAttribute('open') && !dropdown.contains(e.target)) {
+            dropdown.removeAttribute('open')
+          }
+        })
+      })
+  }
+  /**
+     * Ferme le dropdown au clic sur la flèche
+     * @return  {void} 
+     */
+  arrowClose() {
+      let closeArrows = document.querySelectorAll('.arrow-up'); 
+      closeArrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+          arrow.closest('details').removeAttribute('open');
+        })
+      })
+  }
 }
