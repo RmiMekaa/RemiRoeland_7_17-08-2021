5e7d939e2c8471452bf4a51eb0ebc99b0e042dbd
Rémi <roeland.remi@yahoo.fr>
2021-09-29 16:32:37 +0200

    Mise à jour des versions pour le benchmark

--- Benchmark/dataManagerV1.js
+++ Benchmark/dataManagerV1.js
@@ -105,7 +105,8 @@ export class DataManagerV1 {
 
     if (this.filters.input.length > 0 && this.resultsBy.input.length == 0) this.results = [];
     else this.results = this.crossResults();
-    console.log(this.results);
+    console.log('V1', this.results);
+
   }
   /**
    * Compare plusieurs tableaux et retourne un tableau contenant les valeurs similaires
